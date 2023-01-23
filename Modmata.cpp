2773c32d8e73520d050677f0b82d0fb7ca4857e3
Iris Astrid <97922141+0x49524953@users.noreply.github.com>
2023-01-22 21:57:15 -0600

    Add Digital & Analog IO functionality

--- Modmata.cpp
+++ Modmata.cpp
@@ -1,51 +1,12 @@
+/** 
+@file Modmata.cpp
+@author Sam Hutcherson
+@date 01/20/23
+@brief Functions to access Modmata commands from arduino .ino files.
+*/
 #include "Modmata.h"
 #include "HardwareSerial.h"
-using namespace modmata;
 
 ModmataClass Modmata;
 
-void ModmataClass::begin(void)
-{
-  mb.config(&Serial, 9600, SERIAL_8N1);
-  mb.setSlaveId(1);
-  
-  for(word addr = 0; addr < TOTAL_PINS; addr++) {
-    pinConfig[addr] = 0;
-    pinState[addr] = false;
-    mb.addHreg(addr);
-    mb.addCoil(TOTAL_PINS+addr);
-  }
-}
-
-// Returns register of address being written
-word ModmataClass::update()
-{
-  word addr = mb.task();
-  if(addr < TOTAL_PINS) {
-    checkPinMode(addr);
-  }
-  else if(addr < 2*TOTAL_PINS) {
-    checkDigitalWrite(addr);
-  }
-  return addr;
-}
-
-void ModmataClass::checkPinMode(word addr)
-{
-  word mode = mb.Hreg(addr);
-  if(pinConfig[addr] != mode) {
-    pinConfig[addr] = mode;
-    pinMode(addr, mode);
-  }
-}
-
-void ModmataClass::checkDigitalWrite(word addr)
-{
-  bool state = mb.Coil(addr);
-  int pin = addr - TOTAL_PINS;
-  if(pinState[pin] != state) {
-    pinState[pin] = state;
-    digitalWrite(pin, state);
-  } 
-}
-
+// Didn't need the rest of that I guess, since I made it inline? *shrugs*
--- Modmata.h
+++ Modmata.h
@@ -1,23 +1,65 @@
-#include "Boards.h"
+#ifndef MODMATA_H
+#define MODMATA_H
 #include "ModbusSerial.h"
+#include "Constants.h"
+#include <inttypes.h>
 
-namespace modmata {
+using namespace std;
 
-class ModmataClass
-{
+class ModmataClass {
   public:
-    void begin();
-    word update();
+    /**
+    Initialize the Modbus/Firmata connection over serial
+    @return void
+    */
+    void begin() {
+      this->mb.config(&Serial, 9600, SERIAL_8N1);
+      this->mb.setSlaveId(1);
+      this->mb.addHreg(0,0);
+      this->mb.addHreg(1,0);
+      this->mb.addHreg(2,0);
+      return;
+    }
+
+
+    /**
+    Close the Modbus/Firmata serial connection
+    @return void
+    */
+    void close() {
+      // TODO: be able to free registers, close connection, etc...
+      return;
+    }
+
+
+    /**
+    Read data in holding registers and execute commands accordingly
+    @return void
+    */
+    void parseCmd() {
+      uint16_t CMD = this->mb.Hreg(0);
+      uint16_t PIN = this->mb.Hreg(1);
+      uint16_t VAL = this->mb.Hreg(2);
+
+      switch (CMD) {
+        case IDLE:                                          break;
+        case PINMODE:         pinMode(PIN, VAL);            break;    // Make sure 'VAL' here... 
+        case DIGITALWRITE:    digitalWrite(PIN, VAL);       break;
+        case DIGITALREAD:     mb.Hreg(2, digitalRead(PIN)); break;
+        case ANALOGREFERENCE: analogReference(VAL);         break;    // ...and here are consistent with acceptable values/named constants
+        case ANALOGWRITE:     analogWrite(PIN, VAL);        break;
+        case ANALOGREAD:      mb.Hreg(2, analogRead(PIN));  break;
+        default: break;
+      }
+
+      mb.Hreg(0,0);   // Set cmd to 'IDLE' before exiting
+      return;
+    }
+
 
   private:
     ModbusSerial mb;
-    word pinConfig[TOTAL_PINS];
-    bool pinState[TOTAL_PINS];
-    void checkPinMode(word addr);
-    void checkDigitalWrite(word addr);
 
 };
 
-}
-
-extern modmata::ModmataClass Modmata;
+#endif
